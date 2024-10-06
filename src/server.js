527bcf3990342c431fc1f8b158ec3dfdc3cb87d5
Vlad Melnyk <vlad22523@gmail.com>
2024-10-06 07:52:59 +0300

    updated messages

--- src/server.js
+++ src/server.js
@@ -29,7 +29,9 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
-      contacts,
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
     });
   });
 
@@ -38,8 +40,16 @@ export const setupServer = () => {
 
     const contact = await getContactsById(contactId);
 
+    if (!contact) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+
     res.status(200).json({
-      contact,
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
